b5ce7fd343422a6144aa945b844f1af622e98de7
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-07-23 09:46:16 -0500

    idk

--- Assignment 2/delayed notification.js
+++
@@ -1,7 +0,0 @@
-function showDelayedNotification() {
-    const delay = prompt('Enter delay in milliseconds:');
-
-    setTimeout(() => {
-        alert('This is a delayed notification!');
-    }, delay);
-}
